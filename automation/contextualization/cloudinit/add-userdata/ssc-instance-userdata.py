5510a351a96fdbd8422b8f93e466a8322c651488
AdamInersjo <Adam.Inersjo.2778@student.uu.se>
2019-09-27 08:16:55 +0200

    Remove specific Floating IP

--- automation/contextualization/cloudinit/add-userdata/ssc-instance-userdata.py
+++ automation/contextualization/cloudinit/add-userdata/ssc-instance-userdata.py
@@ -13,7 +13,7 @@ name = "adinInstance" + datetime.datetime.today().strftime('%Y-%m-%d--%H-%M-%S')
 flavor = "ssc.small" 
 private_net = "SNIC 2019/10-32 Internal IPv4 Network"
 floating_ip_pool_name = "Public External IPv4 network"
-floating_ip = "130.238.28.157"
+floating_ip = None
 image_name = "Ubuntu 16.04 LTS (Xenial Xerus) - latest"
 
 loader = loading.get_plugin_loader('password')
